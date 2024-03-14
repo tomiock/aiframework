665c3fb0acbc7db455ab61cc1c0162c459879a0e
tomiock <ockier1@gmail.com>
2024-03-14 16:42:59 +0100

    the code is still broken at INIT of Point type
    things changed (from memory):
    - now with Python3.12 always
    - final module declarations as deref
    - `pytfromlong` is taken out (may be problem)
    
    takeaways:
    - imcopatibility between Python Objects given as input and the C types
      themselfs
    - there is not garbage collection implemented
    - need to figure out how to get `python int` as input to type
    - print a pointer of a python object

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import aiframework as ai
 
-i = ai.Point(0, 0)
+# if we give as input (0, 0) it says: returned a result with an exception set (SystemError: <built-in function Point>)
+
+i = ai.Point(1, 0)
 print(i)
