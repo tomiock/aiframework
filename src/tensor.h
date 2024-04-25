265653a6dcffc5166b5f62c36f8ac76e5240fd60
tomiock <ockier1@gmail.com>
2024-04-25 12:20:10 +0200

    merge dev into main

---
+++ src/tensor.h
@@ -0,0 +1,23 @@
+#ifndef TENSOR_H
+#define TENSOR_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct {
+	char dim;
+	unsigned short* shape;
+	unsigned short stride;
+	char type;
+	float* data;
+} Tensor;
+
+
+Tensor * tensor_create(char dim, unsigned short shape[], float data[]);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TENSOR_H
